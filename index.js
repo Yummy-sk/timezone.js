58ae5694b50d814bc0bc50fa654142a3e88d9813
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-05-31 16:26:03 +0900

    feat: export modules

---
+++ index.js
@@ -0,0 +1,9 @@
+export {
+    getCurrentTime,
+    getTimeOfTimeZone,
+    getTimeDifference,
+    getCustomTimeDifference,
+    getDate
+} from './script/index.js';
+
+export * as timzone from './script/index.js';
