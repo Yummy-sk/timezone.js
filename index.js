b657ee01cb530cc09aa3ae44d7e4e1d96faa5d7e
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-06-03 17:30:14 +0900

    feat: add features

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 export {
-    getCurrentTime,
-    getTimeOfTimeZone,
-    getTimeDifference,
-    getCustomTimeDifference,
-    getDate
+  getCurrentTime,
+  getTimeOfTimeZone,
+  getTimeDifference,
+  getTimeOfTimeZone,
+  getCustomTimeDifference,
+  getDate,
+  getCurrentTimeZone,
 } from './script/index.js';
 
 export * as timzone from './script/index.js';
--- script/index.js
+++ script/index.js
@@ -1,19 +1,19 @@
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc.js";
-import timezone from "dayjs/plugin/timezone.js";
-import calendar from "dayjs/plugin/calendar.js";
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc.js';
+import timezone from 'dayjs/plugin/timezone.js';
+import calendar from 'dayjs/plugin/calendar.js';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(calendar);
 
-const defaultFormat = 'YYYY-MM-DD HH:mm'
+const defaultFormat = 'YYYY-MM-DD HH:mm';
 
 /**
  * getCurrentTime
- * 
+ *
  * The time of the current time zone
- * 
+ *
  * @param { string } format
  * @returns { string }
  */
@@ -21,68 +21,94 @@ export const getCurrentTime = (format = defaultFormat) => dayjs().format(format)
 
 /**
  * getTimeOfTimeZone
- * 
+ *
  * Returns the time of the time zone.
- * 
- * @param { string } timezone 
+ *
+ * @param { string } timezone
  * @param { string } format
  * @returns { string }
  */
 export const getTimeOfTimeZone = (timezone, format = defaultFormat) => {
-    const currentTime = new Date();
+  const currentTime = new Date();
 
-    return dayjs(currentTime).tz(timezone).format(format);
-}
+  return dayjs(currentTime).tz(timezone).format(format);
+};
+
+/**
+ * getTimeOfTimeZoneByDate
+ *
+ * Returns the time of the time zone by date.
+ *
+ * @param {string} timezone
+ * @param {string} date
+ * @param {string} format
+ * @returns
+ */
+
+export const getTimeOfTimeZoneByDate = (timezone, date, format = defaultFormat) => {
+  const currentTime = new Date(date);
+
+  return dayjs(currentTime).tz(timezone).format(format);
+};
 
 /**
  * getTimeDifference
- * 
+ *
  * Returns the time difference with the current time zone.
- * 
+ *
  * @param { string } timzone
  * @param { string } format
  * @returns { string }
  */
 export const getTimeDifference = (timzone, format = defaultFormat) => {
-    const currentTime = dayjs().format(format);
-    const timezoneTime = dayjs(currentTime).tz(timzone).format(format);
+  const currentTime = dayjs().format(format);
+  const timezoneTime = dayjs(currentTime).tz(timzone).format(format);
 
-    return dayjs(timezoneTime).diff(currentTime, 'hour');
+  return dayjs(timezoneTime).diff(currentTime, 'hour');
 };
 
 /**
  * getCustomTimeDifference
- * 
+ *
  * Returns the user custom time difference.
- * 
+ *
  * @param { string } standardTimezone
  * @param { string } targetTimezone
  * @param { string } format
  * @returns { string }
  */
 
-export const getCustomTimeDifference = (standardTimezone, targetTimezone, format = defaultFormat) => { 
-    const currentTime = dayjs().format(format);
-    const standardTimezoneTime = dayjs(currentTime).tz(standardTimezone).format(format);
-    const targetTimezoneTime = dayjs(currentTime).tz(targetTimezone).format(format);
+export const getCustomTimeDifference = (standardTimezone, targetTimezone, format = defaultFormat) => {
+  const currentTime = dayjs().format(format);
+  const standardTimezoneTime = dayjs(currentTime).tz(standardTimezone).format(format);
+  const targetTimezoneTime = dayjs(currentTime).tz(targetTimezone).format(format);
 
-    return dayjs(targetTimezoneTime).diff(standardTimezoneTime, 'hour');
-}
-
+  return dayjs(targetTimezoneTime).diff(standardTimezoneTime, 'hour');
+};
 
 /**
  * getDate
- * 
+ *
  * Returns whether the time zone is today, tomorrow, or yesterday based on the current time.
- * 
+ *
  * @param { string } timezone
  * @param { string } format
  * @returns { string }
  */
 
 export const getDate = (timezone, format = defaultFormat) => {
-    const currentTime = dayjs().format(format);
-    const timezoneTime = dayjs(currentTime).tz(timezone).format(format);
+  const currentTime = dayjs().format(format);
+  const timezoneTime = dayjs(currentTime).tz(timezone).format(format);
 
-    return dayjs(timezoneTime).calendar();
-}
+  return dayjs(timezoneTime).calendar();
+};
+
+/**
+ * getCurrentTimeZone
+ *
+ * @returns { string }
+ */
+
+export const getCurrentTimeZone = () => {
+  return dayjs.tz.guess();
+};
